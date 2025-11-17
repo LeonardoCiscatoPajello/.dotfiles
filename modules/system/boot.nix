95c2b07caf3e8ed257b4f21ce4ee777134f6571e
Leonardo Ciscato Pajello <leonardo.pajello@gmail.com>
2025-11-17 16:21:12 +0100

    Update boot to Grub

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,10 +1,19 @@
 { ... }:
 {
-  boot.loader.systemd-boot = {
+  # Use GRUB2 as bootloader instead of systemd-boot
+  boot.loader.grub = {
     enable = true;
-    configurationLimit = 10;  # Keep last 10 generations
-    editor = false;  # Security
+    device = "nodev";  # Don't install to MBR (we're using EFI)
+    efiSupport = true;
+    efiInstallAsRemovable = false;
+    useOSProber = true;  # Detect other OSes (Fedora)
+    configurationLimit = 10;  # Keep last 10 generations (same as before)
+    
+    # Optional: Customize GRUB appearance
+    # theme = null;  # Use default theme
+    # splashImage = null;
   };
+  
   boot.loader.efi.canTouchEfiVariables = true;
   
   # Faster boot
